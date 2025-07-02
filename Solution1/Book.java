2e62fe539fc62e7559607cfd439200e58ba8d48d
nawfal-code <nawfal.mohammed006@gmail.com>
2025-07-02 18:57:04 +0530

    Create Book.java

---
+++ Solution1/Book.java
@@ -0,0 +1,61 @@
+package guvi.task3;
+
+public class Book {
+
+    // Attributes
+    int bookID;
+    String title;
+    String author;
+    boolean isAvailable;
+
+    // Constructor to initialize book attributes
+    public Book(int bookID, String title, String author, boolean isAvailable) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    // Default constructor (Optional, only if needed)
+    public Book() {
+    }
+
+    // Getter and setter methods for book attributes
+    public int getBookID() {
+        return bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    // Overriding toString method for better output formatting
+    @Override
+    public String toString() {
+        return "Book ID: " + bookID + ", Title: " + title + ", Author: " + author + ", Available: " + isAvailable;
+    }
+}
